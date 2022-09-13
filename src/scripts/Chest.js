4eac318418a72f73678cf0dc0603a646864c69ca
andrey1303110 <andrey1303110@gmail.com>
2022-09-13 18:07:16 +0300

    add functionality for set win/lose/bonus win on click Chest, add animation on set positions

--- src/scripts/Chest.js
+++ src/scripts/Chest.js
@@ -1,87 +1,70 @@
 import * as PIXI from "pixi.js";
 import TWEEN from "@tweenjs/tween.js";
+import { ChestGridConfig } from "./ChestGridConfig";
 import { Globals } from "./Globals";
 import {Howl, Howler} from 'howler';
 
-export class Chest extends PIXI.utils.EventEmitter {
-    constructor(id, field) {
-        super();
+export class Chest {
+    constructor(isWin, bonusWin, field, num) {
         this.sprite = new PIXI.Sprite(Globals.resources['chestLose_1'].texture);
+        this.sprite.tint = 0xC9C9C9;
         this.sprite.anchor.set(.5);
-        this.sprite.width = document.body.clientWidth / 7;
-        this.sprite.height = document.body.clientWidth / 7;
+        
+        let ratio = this.sprite.width / this.sprite.height;
+
+        this.sprite.width = ChestGridConfig.chestSize;
+        this.sprite.height = this.sprite.width * ratio;
+
+        this.isOpened = false;
+        this.isWin = Boolean(isWin);
+        this.bonusWin = bonusWin;
         this.field = field;
-        this.sprite.correctId = id;
-        this.reset();
+        this.num = num;
+        this.setPosition();
 
         this.setInteractive();
     }
 
     setInteractive() {
         this.sprite.interactive = true;
-        this.sprite.on('pointerdown', this.onTouchStart, this);
-        this.sprite.on('pointerup', this.onTouchEnd, this);
-        this.sprite.on('pointermove', this.onTouchMove, this);
+        this.sprite.on('pointerdown', this.onTouch, this);
         this.sprite.on('pointerover', this.onPointerOver);
         this.sprite.on('pointerout', this.onPointerOut);
     }
 
-    onTouchStart(event) {
+    onTouch() {
+        if (this.isWin) {
+            alert('you win!')
+        } else {
+            alert('you lose!')
+        }
+        if (this.bonusWin) {
+            alert('Bonus!')
+        }
         this.sprite.zIndex = 1;
-        this.touchPosition = {
-            x: event.data.global.x,
-            y: event.data.global.y,
-        };
-
-        this.dragging = true;
-        Globals.resources.sounds.click.play();
-    }
-
-    onTouchEnd() {
-        this.dragging = false;
-        this.emit('dragend');
-    }
-
-    onTouchMove(event) {
-        if (!this.dragging) {
-            return;
-        }
-
-        const currentPosition = {
-            x: event.data.global.x,
-            y: event.data.global.y,
-        }
-
-        const offsetX = currentPosition.x - this.touchPosition.x;
-        const offsetY = currentPosition.y - this.touchPosition.y;
-
-        this.sprite.x = this.field.x + offsetX;
-        this.sprite.y = this.field.y + offsetY;
     }
     
     onPointerOver(){
-        this.width *= 1.25;
-        this.height *= 1.25;
+        Globals.resources.sounds.click.play();
+        this.tint = 0xFFFFFF;
+        this.width *= 1.225;
+        this.height *= 1.225;
     }
 
     onPointerOut(){
-        this.width /= 1.25;
-        this.height /= 1.25;
+        this.tint = 0xC9C9C9;
+        this.width /= 1.225;
+        this.height /= 1.225;
     }
 
-    reset() {
+    setPosition() {
         const tween = new TWEEN.Tween(this.sprite);
+        const delay = this.num * ChestGridConfig.animation.position_duration;
         tween.to({ 
             x: this.field.x,
             y: this.field.y,
-        }, 325);
-        tween.onStart(() => {
-            this.sprite.zIndex = 1;
-        });
-        tween.onComplete(() => {
-            this.sprite.zIndex = 0;
-        });
-        tween.easing(TWEEN.Easing.Back.Out);
+        }, ChestGridConfig.animation.init_delay + delay);
+        tween.easing(TWEEN.Easing.Exponential.InOut);
         tween.start();
     }
 }
--- src/scripts/ChestGrid.js
+++ src/scripts/ChestGrid.js
@@ -8,54 +8,79 @@ import { Chest } from "./Chest";
 export class ChestGrid {
     constructor() {
         this.container = new PIXI.Container();
-        this.container.x = document.body.clientWidth / 2;
-        this.container.y = document.body.clientHeight / 3;
+        this.container.x = document.body.clientWidth * .5;
+        this.container.y = document.body.clientHeight * .275;
         this.container.sortableChildren = true;
         this.createChests();
     }
 
     createChests(){
-        this.pieces = [];
+        this.chests = [];
 
-        let ids = ChestGridConfig.pieces.map(field => field.id);
+        let bonus_count = 0;
+        const bonus_num = Math.round(Math.random() / ChestGridConfig.bonusWinChance);
 
-        ChestGridConfig.pieces.forEach(field => {
-            const random = Math.floor(Math.random() * ids.length);
-            const id = ids[random];
-            ids = ids.filter(item => item !== id);
+        ChestGridConfig.chests.forEach(field => {
+            const isWin = Math.round(Math.random());
+            let isBonus = false;
+            
+            if (isWin) {
+                if (bonus_count < ChestGridConfig.bonusMaxCount) {
+                    isBonus = this.generateBonusWin(bonus_num);
+                    if (isBonus) {
+                        ++bonus_count;
+                        console.log("We have bonus chest on screen!\nHe number is: " + (Number(this.chests.length) + 1));
+                    }   
+                }
+            }
 
-            const piece = new Chest(id, field, this.container);
-            piece.on('dragend', () => this.onPieceDragEnd(piece));
+            const chest = new Chest(isWin, isBonus, field, this.chests.length);
 
-            this.container.addChild(piece.sprite);
-            this.pieces.push(piece);
+            this.container.addChild(chest.sprite);
+            this.chests.push(chest);
         });
 
-        for (let i = 0; i < this.container.children.length; i++) {
-            const piece = this.container.children[i];
-    
-            const tween = new TWEEN.Tween(this.container);
-            tween.to({ 
-                y: document.body.clientHeight / 2,
-            }, 625);
-            tween.delay(425);
-            tween.easing(TWEEN.Easing.Quadratic.InOut);
-            tween.start();
-        }
+        this.checkIsHaveWinChest();
+
+        const tween = new TWEEN.Tween(this.container);
+        tween.to({ 
+            y: document.body.clientHeight / 2,
+        }, 725);
+        tween.delay(this.chests.length * ChestGridConfig.animation.position_duration);
+        tween.easing(TWEEN.Easing.Bounce.Out);
+        tween.start();
     }
 
-    onPieceDragEnd(piece) {
-        piece.reset();
+    generateBonusWin(bonus_num){
+        return (bonus_num === (Math.round(Math.random() / ChestGridConfig.bonusWinChance)));
+    }
+
+    checkIsHaveWinChest(){
+        let count = 0;
+        this.chests.forEach(chest => {
+            if (chest.isWin) ++count;
+        });
+
+        if (!count) {
+            const random_num = Math.floor(Math.random() * 6);
+            this.chests[random_num].isWin = true;
+
+            const bonus_num = Math.round(Math.random() / ChestGridConfig.bonusWinChance);
+            const isBonus = this.generateBonusWin(bonus_num);
+            if (isBonus) {
+                this.chests[random_num].bonusWin = true;
+            }
+        }
     }
 
     checkPieces(){
         let correct_count = 0;
-        for (let i = 0; i < this.container.children.length; i++) {
-            const piece = this.container.children[i];
+        for (let i = 0; i < this.chests.length; i++) {
+            const piece = this.chests[i];
     
             if (piece.y === ChestGridConfig.pieces[piece.correctId-1].y && piece.x === ChestGridConfig.pieces[piece.correctId-1].x) {
                 correct_count++;
-                if (correct_count >= this.container.children.length) {
+                if (correct_count >= this.chests.length) {
                     Globals.resources.sounds.win.play();
                     this.puzzleDone();
                 }
@@ -64,8 +89,8 @@ export class ChestGrid {
     }
 
     puzzleDone(){
-        for (let i = 0; i < this.container.children.length; i++) {
-            const piece = this.container.children[i];
+        for (let i = 0; i < this.chests.length; i++) {
+            const piece = this.chests[i];
 
             const tween = new TWEEN.Tween(piece);
             let pos = { x: 0, y: 0};
