b5adb8a4f809f0255d18ba231547d5546351ff3c
andrey1303110 <andrey1303110@gmail.com>
2022-09-14 21:31:11 +0300

    add resources

--- src/scripts/LoaderConfig.js
+++ src/scripts/LoaderConfig.js
@@ -7,10 +7,17 @@ export let LoaderConfig = {
     theme_wav: require("../sounds/theme.wav"),
     click_mp3: require("../sounds/click.mp3"),
     click_wav: require("../sounds/click.wav"),
+    coins_mp3: require("../sounds/coins.mp3"),
+    coins_wav: require("../sounds/coins.wav"),
+    congratulations_mp3: require("../sounds/congratulations.mp3"),
+    congratulations_wav: require("../sounds/congratulations.wav"),
     angelic_choir_mp3: require("../sounds/angelic_choir.mp3"),
     angelic_choir_wav: require("../sounds/angelic_choir.wav"),
     bg: require("../sprites/bg.png"),
     play_now: require("../sprites/play_now.png"),
+    bonus: require("../sprites/bonus.png"),
+    bonus_banknote: require("../sprites/bonus_banknote.png"),
+    rays_bg: require("../sprites/rays_bg.png"),
 };
 
 for (let i = 1; i <= 9; i++) {
