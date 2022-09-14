ccfd3e28a734dd78dcf95de470e7b792bafe6fed
andrey1303110 <andrey1303110@gmail.com>
2022-09-14 16:43:40 +0300

    add scenes to global object

--- src/scripts/App.js
+++ src/scripts/App.js
@@ -2,16 +2,15 @@ import * as PIXI from "pixi.js";
 import TWEEN from "@tweenjs/tween.js";
 import { Loader } from "./Loader";
 import { MainScene } from "./MainScene";
+import { Globals } from "./Globals";
 
 export class App {
     run() {
-        // create canvas
         this.app = new PIXI.Application({
             resizeTo: window
         });
         document.body.appendChild(this.app.view);
 
-        // load sprites
         this.loader = new Loader(this.app.loader);
         this.loader.preload().then(() => this.start());
     }
@@ -23,5 +22,9 @@ export class App {
 
         this.scene = new MainScene();
         this.app.stage.addChild(this.scene.container);
+
+        Globals.scenes = {
+            main: this.scene,
+        }
     }
 }
